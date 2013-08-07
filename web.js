52cfe48b4e966204bda150d25e957a70e5e265b0
Nitin Kumar <nitin.nitcalicut@gmail.com>
2013-08-07 15:49:39 +0000

    updated

--- web.js
+++ web.js
@@ -2,9 +2,9 @@ var express = require('express');
 var fs = require('fs');
 var app = express();
 app.use(express.logger());
-//var fileContent = fs.readFileSync('index.html','utf8');
+
 app.get('/', function(request, response) {
-  response.send("Hello World);
+  response.send("Hello World");
 });
 
 var port = process.env.PORT || 5000;
